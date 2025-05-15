d89aef978fbdad8434509d8e316c8da4eddf5712
D24125405 <elkhen.gal@gmail.com>
2025-05-15 13:40:42 +0100

    Main

---
+++ exam2025/destroyer.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var speed = 600
+@export var rotation_speed = 1.5
+
+var rotation_direction = 0
+
+
+
+func get_input():
+	rotation_direction = Input.get_axis("left", "right")
+	velocity = transform.x * Input.get_axis("down", "up") * speed
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_direction * rotation_speed * delta
+	move_and_slide()
+
+
+
+	
